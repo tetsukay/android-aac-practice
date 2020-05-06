3be7ad687b6db96ed82cf4d311b2fdd92895cc77
tetsukay <yuta.tetsuka@gmail.com>
2020-05-06 19:19:49 +0900

    SearchFragmentへのNavigationまで

---
+++ app/src/main/java/app/tetsukay/githubbrowser/ui/search/SearchFragment.kt
@@ -0,0 +1,7 @@
+package app.tetsukay.githubbrowser.ui.search
+
+import androidx.fragment.app.Fragment
+import app.tetsukay.githubbrowser.R
+
+class SearchFragment: Fragment(R.layout.search_fragment) {
+}
