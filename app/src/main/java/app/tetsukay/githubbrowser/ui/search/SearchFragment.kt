884aa039ce9e0a4bc6404f783537aa5be79fed12
tetsukay <yuta.tetsuka@gmail.com>
2020-05-06 19:37:33 +0900

    DataBindingの有効化

--- app/src/main/java/app/tetsukay/githubbrowser/ui/search/SearchFragment.kt
+++ app/src/main/java/app/tetsukay/githubbrowser/ui/search/SearchFragment.kt
@@ -1,7 +1,28 @@
 package app.tetsukay.githubbrowser.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import app.tetsukay.githubbrowser.R
+import app.tetsukay.githubbrowser.databinding.SearchFragmentBinding
 
-class SearchFragment: Fragment(R.layout.search_fragment) {
+class SearchFragment : Fragment(R.layout.search_fragment) {
+
+    private lateinit var binding: SearchFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.search_fragment,
+            container,
+            false
+        )
+        return binding.root
+    }
 }
