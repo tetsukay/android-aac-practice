0ec2fa196ce3d2b92d03ff1704c4868cf6e9bd63
tetsukay <yuta.tetsuka@gmail.com>
2020-05-18 01:49:48 +0900

    notify result from ViewModel

--- app/src/main/java/app/tetsukay/githubbrowser/ui/search/SearchViewModel.kt
+++ app/src/main/java/app/tetsukay/githubbrowser/ui/search/SearchViewModel.kt
@@ -1,14 +1,22 @@
 package app.tetsukay.githubbrowser.ui.search
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import app.tetsukay.githubbrowser.api.response.GitHubRepoSearchResponse
+import app.tetsukay.githubbrowser.model.Resource
+import app.tetsukay.githubbrowser.repository.GitHubRepository
 import java.util.*
 
 class SearchViewModel : ViewModel() {
 
+    private val repository = GitHubRepository()
     private val _query = MutableLiveData<String>()
-    val query: LiveData<String> = _query
+    val searchResult: LiveData<Resource<GitHubRepoSearchResponse>> = _query.switchMap {
+        liveData<Resource<GitHubRepoSearchResponse>> {
+            emitSource(
+                repository.search(it).asLiveData()
+            )
+        }
+    }
 
     fun setQuery(originalInput: String) {
         val input = originalInput.toLowerCase(Locale.getDefault()).trim()
