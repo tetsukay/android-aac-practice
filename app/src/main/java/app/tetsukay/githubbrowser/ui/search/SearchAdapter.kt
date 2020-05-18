7e74e5dddc59ed588e9585415f0dc109a487ae40
tetsukay <yuta.tetsuka@gmail.com>
2020-05-19 01:19:27 +0900

    implementation SearchAdapter

--- app/src/main/java/app/tetsukay/githubbrowser/ui/search/SearchAdapter.kt
+++ app/src/main/java/app/tetsukay/githubbrowser/ui/search/SearchAdapter.kt
@@ -1,13 +1,37 @@
 package app.tetsukay.githubbrowser.ui.search
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import app.tetsukay.githubbrowser.R
 import app.tetsukay.githubbrowser.databinding.SearchListItemBinding
 import app.tetsukay.githubbrowser.model.GitHubRepo
 
-class SearchAdapter(
-    private val data: List<GitHubRepo>
-) : RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+class SearchAdapter : RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
+
+    private var data: List<GitHubRepo> = listOf()
 
     inner class ViewHolder(val binding: SearchListItemBinding) :
         RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.search_list_item,
+                parent, false
+            )
+        )
+
+    override fun getItemCount(): Int = data.size
+
+    fun setData(data: List<GitHubRepo>) {
+        this.data = data
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.repo = data[position]
+    }
 }
